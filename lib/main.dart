fc0880be44f6f444ac4cb20e8da9cb9da5c88a66
DiiWarsame <diiwarsame143@gmail.com>
2022-10-12 22:21:50 +0200

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,34 @@ void main(){
       backgroundColor: Colors.yellow,
       centerTitle: true,
       ),
+
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                margin: EdgeInsets.all(10),
+                color: Colors.red,
+                child: Row(
+                  children: <Widget>[
+                    Image(image: AssetImage('assets/img/fruit.png'),
+                    width: 100,
+                    height: 100,
+                    ),
+                    SizedBox(width: 20),
+                    Text('This Fruit Used for Somalian Peoples', style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     ),
   ));
 }
